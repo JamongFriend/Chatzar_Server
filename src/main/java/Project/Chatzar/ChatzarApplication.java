6a2ec2506c19990e83b737682c8d81c445476553
JamogFrinds <ghrb6769@gmail.com>
2025-11-25 14:19:53 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/Project/Chatzar/ChatzarApplication.java
@@ -0,0 +1,13 @@
+package Project.Chatzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatzarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/Chatzar/ChatzarApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.Chatzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
