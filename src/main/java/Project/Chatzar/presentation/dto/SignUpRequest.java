e6943cd999cefb075d3403d47e1d0f86a5f25e75
JamogFrinds <ghrb6769@gmail.com>
2025-12-18 11:34:58 +0900

    refactor: Request Dto 변경

--- src/main/java/Project/Chatzar/presentation/dto/SignUpRequest.java
+++ src/main/java/Project/Chatzar/presentation/dto/SignUpRequest.java
@@ -2,10 +2,12 @@ package Project.Chatzar.presentation.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
-public record LoginRequest(
+public record SignUpRequest(
+        @NotBlank String name,
         @Email @NotBlank String email,
-        @NotBlank String password
+        @NotBlank String password,
+        @NotBlank String nickname,
+        @NotBlank String age
+
 ) {}
