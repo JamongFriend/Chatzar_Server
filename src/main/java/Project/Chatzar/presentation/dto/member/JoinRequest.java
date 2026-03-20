20f06086df8bbc3e1d9ae33d0d457255a31d98d4
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2026-03-20 15:16:47 +0900

    fix: @NotBlank -> @NotNull 수정

--- src/main/java/Project/Chatzar/presentation/dto/member/JoinRequest.java
+++ src/main/java/Project/Chatzar/presentation/dto/member/JoinRequest.java
@@ -2,11 +2,12 @@ package Project.Chatzar.presentation.dto.member;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record JoinRequest(
         @NotBlank String name,
         @Email @NotBlank String email,
         @NotBlank String password,
         @NotBlank String nickname,
-        @NotBlank Long age
+        @NotNull Long age
 ) {}
